efe6457da7c2ef49f06f91ef361b2efebd1dfa90
YaaseenIbrahim <yaseenibrahimm12@gmail.com>
2024-04-22 15:49:14 +0500

    Dropdown menu for writings heading created

--- resources.js
+++
@@ -1,6 +0,0 @@
-const Labels = document.querySelectorAll('.label');
-const Input = document.querySelectorAll('.accordian__input')
-for (let i = 0; i < Labels.length; i++) {
-    Labels[i].setAttribute('for', 'title' + i)
-    Input[i].setAttribute('id', 'title' + i)
-}
--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // Close button instances
 var modalArea = document.getElementById("modal__area");
 const sidemenuArea = document.getElementById('sidemenu--area')
-console.log(sidemenuArea)
 
 function closeThing(thingtoclose) {
     document.body.style.overflow = 'auto'
@@ -19,3 +18,26 @@ function showSidemenu(){
     document.body.classList.add('darken__filter')
 }
 
+(function ($) {
+    $(document).ready(function () {
+
+        $('#cssmenu li.has-sub>a').on('click', function () {
+            $(this).removeAttr('href');
+            var element = $(this).parent('li');
+            if (element.hasClass('open')) {
+                element.removeClass('open');
+                element.find('li').removeClass('open');
+                element.find('ul').slideUp(200);
+            }
+            else {
+                element.addClass('open');
+                element.children('ul').slideDown(200);
+                element.siblings('li').children('ul').slideUp(200);
+                element.siblings('li').removeClass('open');
+                element.siblings('li').find('li').removeClass('open');
+                element.siblings('li').find('ul').slideUp(200);
+            }
+        });
+
+    });
+})(jQuery);
